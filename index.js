48e2d3b414082c72afa80bc2e39083c54b8c0696
aMarCruz <amarcruz@yahoo.com>
2018-03-06 17:15:02 -0600

    Usa ES6 en el index

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-var react = require('react-native');
-var Dimensions = react.Dimensions;
-var RNMeasureText = react.NativeModules.RNMeasureText;
+import { Dimensions, NativeModules } from 'react-native';
+
+const RNMeasureText = NativeModules.RNMeasureText;
 
 /**
  * Get width, height, lineCount, and lastLineWidth values for text.
  *
  * @param {object} params - Info
  */
-exports.measure = function measure (params) {
+export function measure (params) {
 
-  var fontSize = params && params.fontSize
+  const fontSize = params && params.fontSize
 
   if (typeof fontSize !== 'number') {
     return Promise.reject(new Error('missing required \'fontSize\' property'));
   }
 
-  var fontScale = Dimensions.get('window').fontScale || 1;
+  const fontScale = Dimensions.get('window').fontScale || 1;
 
   if (fontScale !== 1) {
     params = Object.assign(
